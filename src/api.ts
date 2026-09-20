0efd0421e9c337fd559631006329ed2a6991d3c5
hozlucas28 <hozlucas28@gmail.com>
2026-09-20 17:24:03 -0300

    feat(api): apply helmet and compression middlewares

--- src/api.ts
+++ src/api.ts
@@ -1,4 +1,6 @@
+import compression from "compression"
 import express from "express"
+import helmet from "helmet"
 import { v1Router } from "./v1/routes/index.ts"
 import { API_PORT } from "./env.ts"
 import { httpLogger, logger } from "./shared/logger.ts"
@@ -10,8 +12,9 @@ const api: Express = express()
 
 // Middlewares
 api.use(httpLogger)
+api.use(helmet())
+api.use(compression())
 api.use(express.json())
 api.use("/api/v1", v1Router)
-api.use("/api/v1", errorHandler)
 
 api.listen(API_PORT, () => logger.info({ port: API_PORT }, "Server listening"))
--- src/v1/routes/index.ts
+++ src/v1/routes/index.ts
@@ -1,9 +1,11 @@
 import { Router } from "express"
+import { errorHandler } from "../middlewares/error-handler.middleware.ts"
 import { productsRouter } from "./products.routes.ts"
 
 const v1Router = Router()
 
 // Middlewares
 v1Router.use("/products", productsRouter)
+v1Router.use(errorHandler)
 
 export { v1Router }
